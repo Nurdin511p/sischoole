80e21a1924e85a1685d356a3f54a75d6e139a103
Nurdin <nurdin511p@gmail.com>
2026-05-08 20:50:56 +0700

    add auth pages

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,39 @@
+async function register() {
+  const email = document.getElementById("register-email").value;
+
+  const password = document.getElementById("register-password").value;
+
+  const { data, error } = await supabaseClient.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Register berhasil!");
+
+  window.location.href = "login.html";
+}
+
 async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById("login-email").value;
+
+  const password = document.getElementById("login-password").value;
 
   const { data, error } = await supabaseClient.auth.signInWithPassword({
     email,
     password,
   });
 
-  console.log(data);
-  console.log(error);
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Login berhasil!");
+
+  window.location.href = "dashboard.html";
 }
